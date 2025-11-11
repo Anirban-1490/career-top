e7072f38fe98129deafc86aed608b391da5cbfc9
Anirban-1490 <anirbanpratihar45@gmail.com>
2025-11-11 14:35:57 +0530

    feat: added header and main hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      <header className="h-lg  ">
+        <div className=" h-full mx-20 flex items-center gap-4">
+          <h2 className=" font-bold text-md">CareerTOP</h2>
+          <div>
+            <button></button>
+          </div>
+          <div></div>
+        </div>
+        <nav></nav>
+      </header>
+      <section className=" py-12">
+        <div>
+          <h1 className=" text-xxl font-light text-center">
+            No Stress Job Hunting
+            <div className=" font-bold">HUNT • APPLY • INTERVIEW</div>
+          </h1>
+          <p></p>
+        </div>
+      </section>
+    </main>
   );
 }
