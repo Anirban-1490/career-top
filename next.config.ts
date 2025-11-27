dc55b3b51ede57b59ab82695611e14911669845d
Anirban-1490 <anirbanpratihar45@gmail.com>
2025-11-27 20:56:49 +0530

    feat: search bar added to search max 3 jobs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+        port: "",
+        pathname: "/images**",
+      },
+    ],
+  },
   /* config options here */
 };
 
