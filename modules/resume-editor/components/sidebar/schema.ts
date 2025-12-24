1825e9d5e8aefde77aeb0361156742098e8128b4
Anirban-1490 <anirbanpratihar45@gmail.com>
2025-12-24 16:24:28 +0530

    chore: updated parent sidebar schema

--- modules/resume-editor/components/sidebar/schema.ts
+++ modules/resume-editor/components/sidebar/schema.ts
@@ -3,15 +3,23 @@ import { personalInfoSchema } from "./components/personal-info/schema";
 import { workInfoSchema } from "./components/work/schema";
 import { socialLinkSchema } from "./components/social-links/schema";
 import { projectSchema } from "./components/projects/schema";
+import { educationSchema } from "./components/education/schema";
+import { achievementsSchema } from "./components/achievements/schema";
 
 export const resumeSchema = z.object({
   personalInformation: personalInfoSchema,
   socialLinks: socialLinkSchema,
   //   professionalSummary: z.string().max(1000).optional(),
   workExperience: z.array(workInfoSchema),
-  //   skillsAndInterests: skillsAndInterestsSchema,
+  skills: z.array(
+    z.object({
+      id: z.string(),
+      label: z.string(),
+    })
+  ),
   projects: z.array(projectSchema),
-  //   awardsAndAchievements: z.array(awardSchema),
+  education: z.array(educationSchema),
+  achievements: z.array(achievementsSchema),
 });
 
 export type ResumeOutputType = z.infer<typeof resumeSchema>;
