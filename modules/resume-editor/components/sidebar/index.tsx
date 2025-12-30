d82b55289a55b2d70d8bebc54c15eb61a54e07da
Anirban-1490 <anirbanpratihar45@gmail.com>
2025-12-30 17:18:29 +0530

    style: editor sidebar preservs the scroll width

--- modules/resume-editor/components/sidebar/index.tsx
+++ modules/resume-editor/components/sidebar/index.tsx
@@ -3,7 +3,6 @@
 import React, { JSX } from "react";
 
 import { PersonalInfo } from "./components/personal-info";
-import { Accordion } from "@/components/ui/accordion";
 import { WorkInfo } from "./components/work";
 import { SocialLinks } from "./components/social-links";
 import { Projects } from "./components/projects";
@@ -11,7 +10,6 @@ import { Skills } from "./components/skills";
 import { Education } from "./components/education";
 import { Achievements } from "./components/achievements";
 import { IControlProps } from "./type";
-import { addResume } from "../../actions/save-resume";
 
 const sideBarContent = [
   PersonalInfo,
@@ -31,8 +29,8 @@ interface IEditorSidebarProps {
 
 export function EditorSidebar({ children }: IEditorSidebarProps) {
   return (
-    <aside className="basis-[30%] sticky left-0 top-0 overflow-hidden">
-      <div className=" h-full  flex flex-col gap-8 p-3 pb-7 overflow-auto">
+    <aside className="basis-[30%] sticky left-0 top-0 overflow-hidden ">
+      <div className=" h-full  flex flex-col gap-8 p-3 pb-7 overflow-auto editor_sidebar__inner">
         {children(sideBarContent)}
       </div>
     </aside>
