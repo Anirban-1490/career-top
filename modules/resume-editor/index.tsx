b489e457f5570f0f9d461900aaadf3dd9169f214
Anirban-1490 <anirbanpratihar45@gmail.com>
2026-01-08 16:42:40 +0530

    chore: formprovider has been added to resume editor, refactored some JSX

--- modules/resume-editor/index.tsx
+++ modules/resume-editor/index.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import React, { use } from "react";
+import React, { use, useRef } from "react";
 import { EditorNavbar } from "./components/resume-navbar";
 import { EditorSidebar } from "./components/sidebar";
 import { EditorOutput } from "./components/output";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { ResumeOutputType, resumeSchema } from "./components/sidebar/schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Accordion } from "@/components/ui/accordion";
 
-import { getID } from "@/lib/get-id";
 import { addResume, IAddResumeProps } from "@/action/save-resume";
-import { useFormStatus } from "react-dom";
+
 import { useMutation } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/sonner";
 
 export function Editor({
   dataPromise,
@@ -25,23 +25,31 @@ export function Editor({
 }) {
   const data = use(dataPromise);
 
-  const {
-    handleSubmit,
-    register,
-    reset,
-    watch,
-    control,
-    formState: { errors },
-  } = useForm<ResumeOutputType>({
+  const formMethods = useForm<ResumeOutputType>({
     resolver: zodResolver(resumeSchema),
     defaultValues: data[0],
   });
+  const {
+    handleSubmit,
 
+    control,
+    formState: { errors },
+  } = formMethods;
   const { mutate, isPending } = useMutation({
     mutationFn: async ({ ...props }: IAddResumeProps) => {
       return await addResume({ ...props });
     },
   });
+  const contentRef = useRef<HTMLDivElement>(null);
+  // const printHandler = useReactToPrint({
+  //   contentRef,
+  //   print: async (target) => {
+  //     console.log(target);
+  //     const html2pdf = (await import("html2pdf.js")).default;
+  //     const worker = html2pdf();
+  //     await worker.from(target).set({ filename: "resume.pdf" }).save();
+  //   },
+  // });
 
   const submitHandler = (data: ResumeOutputType) => {
     mutate({ userId: userId, resume: data, resumeId: id });
@@ -49,31 +57,39 @@ export function Editor({
 
   return (
     <main className="h-dvh overflow-hidden flex flex-col">
-      <EditorNavbar />
-      <main className="flex w-full flex-grow  overflow-hidden">
-        <EditorSidebar>
-          {(editorSidebarContent) => {
-            return (
-              <form onSubmit={handleSubmit(submitHandler)}>
-                <Accordion
-                  type="single"
-                  className="flex flex-col gap-5"
-                  collapsible
-                >
-                  {editorSidebarContent.map((Content, index) => {
-                    return (
-                      <React.Fragment key={index}>
-                        <Content isFormPending={isPending} control={control} />
-                      </React.Fragment>
-                    );
-                  })}
-                </Accordion>
-              </form>
-            );
-          }}
-        </EditorSidebar>
-        <EditorOutput control={control} />
-      </main>
+      <EditorNavbar contentRef={contentRef} />
+      <FormProvider {...formMethods}>
+        <main className="flex w-full flex-grow  overflow-hidden">
+          <EditorSidebar>
+            {(editorSidebarContent) => {
+              return (
+                <form onSubmit={handleSubmit(submitHandler)}>
+                  <Accordion
+                    type="single"
+                    className="flex flex-col gap-5"
+                    collapsible
+                  >
+                    {editorSidebarContent.map((Content, index) => {
+                      return (
+                        <React.Fragment key={index}>
+                          <Content
+                            isFormPending={isPending}
+                            control={control}
+                            id={id}
+                            userId={userId}
+                          />
+                        </React.Fragment>
+                      );
+                    })}
+                  </Accordion>
+                </form>
+              );
+            }}
+          </EditorSidebar>
+          <EditorOutput contentRef={contentRef} />
+        </main>
+      </FormProvider>
+      <Toaster richColors position="bottom-right" expand />
     </main>
   );
 }
