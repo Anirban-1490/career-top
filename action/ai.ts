0e1b433650f5effdbb86eec8c626b1f19b7751c2
Anirban-1490 <anirbanpratihar45@gmail.com>
2026-01-10 22:24:28 +0530

    feat: added ai-enhancer dialog box for resumes

---
+++ action/ai.ts
@@ -0,0 +1,91 @@
+"use server";
+
+import { ajlib } from "@/lib/arcjet";
+import { geminiAI } from "@/services/gemini";
+import { ArcjetRateLimitReason } from "@arcjet/next";
+import dayjs from "dayjs";
+import { UserProfile } from "./user-profile";
+
+const AIEnhancerPrompt = (description: string) => `
+Make yourself as a expert resume analyzer who gives very ATS friendly resume.
+Now check this check this description of something the User has worked on, and improve it for ATS friendly
+
+${description}
+
+Put the output in this below format in JSON .
+
+
+Output format - 
+{
+    enhancedText:{output}
+}
+
+
+DO NOT PUT ANY OTHER EXTRA TEXT, JUST OUTPUT ONLY IN THIS PROVIDED FORMAT.
+ALSO KEEP THE SAME TYPE OF HTML MARKUP THAT THE USER HAS PROVIDED AND KEEP IT HTML ORIENTED.
+AND DO NOT PROVIDE ANY EXTRA CONTEXT THAT THE USER HASENT PROVIDED IN ITS DESCRIPTION
+
+`;
+
+export async function AIEnhancer(description: string) {
+  let maxToken, remaining;
+  try {
+    const user = await UserProfile();
+
+    const ajResult = await ajlib({
+      tokenMaxCapacity: 5,
+      tokenInterval: "1d",
+      tokenRefillRate: 1,
+      config: {
+        userId: user.uid,
+      },
+    });
+    (maxToken = ajResult?.maxToken), (remaining = ajResult?.remaining);
+
+    const aiResponse = await geminiAI.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: {
+        text: AIEnhancerPrompt(description),
+      },
+    });
+
+    //* remove the ```json``` from the ai response with a regex
+    const formattedText = aiResponse.text?.match(/\{[^}]*\}/i)?.[0];
+    const jsonOutput = JSON.parse(formattedText as string) as {
+      enhancedText: string;
+    };
+    return {
+      ...jsonOutput,
+      message: "",
+      rateLimit: {
+        maxToken,
+        remaining,
+        hasExceededRateLimit: remaining === 0,
+      },
+    };
+  } catch (error) {
+    if (error instanceof ArcjetRateLimitReason) {
+      //* rate limited user
+      return {
+        enhancedText: "",
+        message: `Oops! Limit Reached. Next refill in ${dayjs(
+          error.resetTime
+        ).diff(dayjs(), "hours")}h`,
+        rateLimit: {
+          maxToken: error.max,
+          remaining: error.remaining,
+          hasExceededRateLimit: error.remaining === 0,
+        },
+      };
+    }
+    return {
+      enhancedText: "",
+      message: "Something went wrong. Please try again!",
+      rateLimit: {
+        maxToken,
+        remaining,
+        hasExceededRateLimit: remaining === 0,
+      },
+    };
+  }
+}
---
+++ components/common/ai-dialog/index.tsx
@@ -0,0 +1,139 @@
+import { AIEnhancer } from "@/action/ai";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useQuery } from "@tanstack/react-query";
+
+import React, { PropsWithChildren, useEffect, useState } from "react";
+import { ContainerWithSpinner } from "../container-with-spinner";
+import { Parser } from "html-to-react";
+import { RefreshCcw } from "lucide-react";
+
+interface AIDialogProps {
+  description: string;
+  addAIEnhancedDescription?: (enhancedText: string) => void;
+  setText?: (value: string) => void;
+}
+
+export function AIDialog({
+  children,
+  description,
+  addAIEnhancedDescription,
+}: AIDialogProps & PropsWithChildren) {
+  const [open, setOpen] = useState(false);
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent showCloseButton={false} className="sm:max-w-[40rem]">
+        <AIDialogInner
+          setOpen={setOpen}
+          description={description}
+          addAIEnhancedDescription={addAIEnhancedDescription}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+function AIDialogInner({
+  description,
+  setOpen,
+  addAIEnhancedDescription,
+}: AIDialogProps & {
+  setOpen: (value: boolean) => void;
+}) {
+  const [text, setText] = useState("");
+  const [hasMounted, setHasMounted] = useState(false);
+
+  const { isFetching, data, refetch } = useQuery({
+    queryKey: ["ai-enchancer"],
+    queryFn: async () => AIEnhancer(description),
+    refetchOnWindowFocus: false,
+
+    gcTime: 0,
+  });
+
+  useEffect(() => {
+    setHasMounted(true);
+
+    return () => setHasMounted(false);
+  }, []);
+  useEffect(() => {
+    if (data?.enhancedText) {
+      setText?.(data?.enhancedText as string);
+    }
+  }, [data?.enhancedText, setText]);
+
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>AI Enhancer</DialogTitle>
+        <DialogDescription>
+          Use our AI agent to improve your resume content into next level
+        </DialogDescription>
+      </DialogHeader>
+      <div
+        id="ai-dialog-output"
+        className=" w-full h-70 border border-input-outline rounded-md overflow-auto p-4 flex flex-col"
+      >
+        {isFetching && (
+          <ContainerWithSpinner loadingLabel={"Improving with AI..."} />
+        )}
+        {!isFetching && data?.message ? (
+          <div className="mt-auto text-center">{data.message}</div>
+        ) : (
+          <div className=" list-disc ">
+            {Parser().parse(data?.enhancedText)}
+          </div>
+        )}
+      </div>
+      <div className="flex justify-end items-center gap-3">
+        <Button
+          onClick={() => {
+            if (isFetching) return;
+            refetch();
+          }}
+          disabled={isFetching || data?.rateLimit.hasExceededRateLimit}
+          aria-disabled={isFetching || data?.rateLimit.hasExceededRateLimit}
+          variant={"ghost"}
+          size={"lg"}
+          className="w-fit h-fit !p-0 disabled:cursor-not-allowed"
+        >
+          {" "}
+          <RefreshCcw className=" size-5" />
+        </Button>
+        <span>
+          Re-Enhance Left: {data?.rateLimit.remaining}/
+          {data?.rateLimit.maxToken}
+        </span>
+      </div>
+      <DialogFooter className="mt-5">
+        <DialogClose asChild>
+          <Button size={"sm"} variant={"ghost"} type="button">
+            Close
+          </Button>
+        </DialogClose>
+        <Button
+          onClick={() => {
+            if (isFetching || data?.message) return;
+            addAIEnhancedDescription?.(text);
+            setOpen(false);
+          }}
+          size={"sm"}
+          disabled={isFetching || !!data?.message}
+          type="button"
+        >
+          Add To Resume
+        </Button>
+      </DialogFooter>
+    </>
+  );
+}
